db6ee6a030e838ab1b3242ae5db9de4eb334c775
Mailon Bruno <mailondecampos@gmail.com>
2022-09-10 12:39:53 -0400

    Criando os serviços e endpoints da Entidade Funcionario

--- src/main/java/com/br/apibarbearia/resource/FuncionarioResource.java
+++ src/main/java/com/br/apibarbearia/resource/FuncionarioResource.java
@@ -2,6 +2,9 @@ package com.br.apibarbearia.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,20 @@ public class FuncionarioResource {
     @PostMapping("/save")
     public ResponseEntity<Funcionario> salvarFuncionario(@RequestBody Funcionario funcionario){
         return ResponseEntity.ok(funcionarioService.salvarFuncionario(funcionario));
+    }
+    
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Funcionario> buscarFuncionario(Long id){
+        return ResponseEntity.ok(funcionarioService.buscarFuncionario(id));
+    }
+    
+    @PostMapping("/updade/{id}")
+    public ResponseEntity<Funcionario> alterarFuncionario(@PathVariable Long id ,@RequestBody Funcionario funcionario){
+        return ResponseEntity.ok(funcionarioService.alterarFuncionario(id ,funcionario));
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Funcionario> removerFuncionario(@PathVariable Long id){
+        return ResponseEntity.ok(funcionarioService.removerFuncionario(id));
     }
 }
--- src/main/java/com/br/apibarbearia/service/FuncionarioService.java
+++ src/main/java/com/br/apibarbearia/service/FuncionarioService.java
@@ -1,5 +1,8 @@
 package com.br.apibarbearia.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,13 +22,37 @@ public class FuncionarioService {
     public Funcionario salvarFuncionario(Funcionario funcionario) {
     	
     	DadosPessoais dadosPessoais = dadosPessoaisService.buscarDadosPessoaisById(funcionario.getDadosPessoais().getId());
-    	
+
     	if(dadosPessoais == null ) {
     		throw new RuntimeException();
     	}
-    	
-    	funcionario.setDadosPessoais(dadosPessoais);;
-    	
+    	funcionario.setDadosPessoais(dadosPessoais);
         return funcionarioRepository.save(funcionario);
+    }
+    
+    public Funcionario buscarFuncionario(Long id) {
+    	return funcionarioRepository.findById(id).get();
+    }
+    
+    public Funcionario alterarFuncionario(Long id,Funcionario funcionario) {
+    	
+    	Optional<Funcionario> funcionarioSalvo = funcionarioRepository.findById(id);
+
+    	if(funcionarioSalvo == null ) {
+    		throw new RuntimeException();
+    	}  	
+    	BeanUtils.copyProperties(funcionario, funcionarioSalvo.get());
+        return funcionarioRepository.save(funcionarioSalvo.get());
+    }
+    
+    public Funcionario removerFuncionario(Long id) {
+    	
+    	Optional<Funcionario> funcionarioSalvo = funcionarioRepository.findById(id);
+
+    	if(funcionarioSalvo == null ) {
+    		throw new RuntimeException();
+    	}
+    	funcionarioRepository.deleteById(id);
+    	return null;
     }
 }
